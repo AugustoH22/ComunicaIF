19e2503e7692204fe0fb0d05ee4128c650e50096
Augusto Braga <augusto.braga@estudante.iftm.edu.br>
2025-04-22 18:45:47 -0300

    Adicionar arquivos pom.xml e Comunica_if.java com configuração inicial do projeto

---
+++ comunica_if/src/main/java/com/pv/comunica_if/Comunica_if.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pv.comunica_if;
+
+/**
+ *
+ * @author augus
+ */
+public class Comunica_if {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
